747f5bb25b0f5c712b62e41283cfbb9ac92bab2f
Sadikur Rahaman <fahim99369@gmail.com>
2025-01-21 03:50:26 +0600

    ide changed

---
+++ CodeWars/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+public class Kata
+{
+    public static string Rot13(string input)
+    {
+        string rot13 = "";
+        foreach (char n in input)
+        {
+            if (n >= 'a' && n <= 'z')
+            {
+                if (n + 13 <= 'z')
+                {
+                    rot13 +=(char)(n + 13);
+                }
+                else
+                {
+                    var overflow = n + 13;
+                    var extra = overflow - 'z';
+                    var offsetFromstart = extra + 'a';
+                    char startincludedoffset = (char)(offsetFromstart - 1);
+                    
+                    rot13 += startincludedoffset;
+                }
+            }
+            else if (n >= 'A' && n <= 'Z')
+            {
+                var overflow = (n - 'A') + 13;
+                var mod = overflow % 26;
+                var final = mod + 'A';
+                
+                rot13 += (char)final;
+            }
+            else
+            {
+                rot13 += n.ToString();
+            }
+        }
+
+        return rot13;
+    }
+    
+}
